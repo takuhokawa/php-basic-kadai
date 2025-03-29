e083267474bbd536cdc5106808574a5acf88a07e
takuhokawa <taku.2263.db@gmail.com>
2025-03-30 00:15:29 +0900

    kadai_005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+
+    // 生徒10人分の点数を入れる変数
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    // 生徒の合計点
+    $score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    // 平均点
+    echo $score / 10;
+    ?>
+  </p>
+</body>
+
+</html>
